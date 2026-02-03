4317f75b94cb3b27962f634419985c11800a71be
Cody <jardinecody@gmail.com>
2026-02-03 17:07:34 -0500

    Initial scaffold

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
 
+const token = process.env.DISCORD_TOKEN;
+if (!token) {
+  console.error("Missing DISCORD_TOKEN env var");
+  process.exit(1);
+}
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("clientReady", () => {
+  console.log(`Togetharr connected as ${client.user.tag}`);
+});
+
+client.login(token);
